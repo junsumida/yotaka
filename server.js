4542ba11a145759cbd85d9573d76108a99ff2ed1
Jun SUMIDA <sjun@wondershake.com>
2012-10-26 15:41:56 +0900

    merged sockserver into railway

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*global require:true*/
 #!/usr/bin/env node
 
 var app = module.exports = require('railway').createServer();
@@ -5,6 +6,18 @@ var app = module.exports = require('railway').createServer();
 if (!module.parent) {
     var port = process.env.PORT || 3000
     app.listen(port);
+    var io = require('socket.io').listen(app.listen(port));
+    var redis = require('redis');
+    var rcli  = redis.createClient();
+
+    rclient.on("ready", function(){
+      console.log("rclient is ready.");
+    });
+
+    rclient.zrange('hage', 0, -1, function(err, mems){
+      console.log(mems);
+    });
+
     console.log("Railway server listening on port %d within %s environment", port, app.settings.env);
 }
 
